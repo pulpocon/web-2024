c03e45afd18fbbd1c38be1321f0c8977e3c7f954
Fran Iglesias <franiglesias@mac.com>
2024-08-11 20:08:23 +0200

    Update maria-larios workshop

--- data/workshops/maria-larios.ts
+++ data/workshops/maria-larios.ts
@@ -1,5 +1,5 @@
 export default {
-    title: 'TBC',
+    title: 'Entendiendo los drivers de nuestro equipo tech: qué les mueve a cambiar y elegir nuevos proyectos',
     speaker: [
         {
             image: 'maria-larios.jpg',
@@ -8,7 +8,7 @@ export default {
         }
     ],
     description: [
-        '',
+        'Como Managers es importante conocer y entender los driver que mueven a nuestras developers a abandonar las empresas y escoger sus nuevas aventuras profesionales. Esto nos permitirá desarrollar estrategias de retención y atracción del talento en nuestros equipos y promoverlas internamente dentro de nuestras organizaciones'
     ],
     schedule: 'Viernes 16:00-18:00',
     track: 'Management',
