3e568cb9292ec2418191b78e81f680fd61da61aa
Rolando Caldas <rolando.caldas@qualifyze.com>
2024-09-08 21:31:57 +0200

    update sponsors

--- data/sponsors.ts
+++ data/sponsors.ts
@@ -21,6 +21,11 @@ const sponsors: SponsorGroups = {
       logo: '/i/sps/elastic.svg',
       link: 'https://www.elastic.co/es/?utm_origin=pulpocon&utm_medium=web'
     },
+    {
+      name: 'Elastic',
+      logo: '/i/sps/edpuzzle.svg',
+      link: 'https://edpuzzle.com/?utm_origin=pulpocon&utm_medium=web'
+    },
   ],
   [SponsorType.Diamond]: [
     {
@@ -57,12 +62,47 @@ const sponsors: SponsorGroups = {
       logo: '/i/sps/ultreia.png',
       link: 'https://www.ultreia.es/?utm_origin=pulpocon&utm_medium=web'
     },
+    {
+      name: 'Vitae',
+      logo: '/i/sps/vitae.png',
+      link: 'https://www.vitaedigital.com'
+    },
+    {
+      name: 'SeQura',
+      logo: '/i/sps/SeQura.svg',
+      link: 'https://www.sequra.com/es?utm_origin=pulpocon&utm_medium=web'
+    },
+    {
+      name: 'Qualifyze',
+      logo: '/i/sps/qualifyzeLogo.png',
+      link: 'https://www.ultreia.es/?utm_origin=pulpocon&utm_medium=web'
+    },
+    {
+      name: 'Sngular',
+      logo: '/i/sps/sngular.svg',
+      link: 'https://www.sngular.com/?utm_origin=pulpocon&utm_medium=web'
+    },
+    {
+      name: 'Mercadona Tech',
+      logo: '/i/sps/mercadona-tech.svg',
+      link: 'https://www.mercadonatech.com/?utm_origin=pulpocon&utm_medium=web'
+    },
+    {
+      name: 'Adevinta',
+      logo: '/i/sps/adevinta.svg',
+      link: 'https://adevinta.com/es/?utm_origin=pulpocon&utm_medium=web'
+    },
+    {
+      name: 'SCRM Lidl International Hub',
+      logo: '/i/sps/scrm.png',
+      link: 'https://scrm.lidl/?utm_origin=pulpocon&utm_medium=web'
+    },
   ],
   [SponsorType.Multimedia]: [
     {
       name: 'Teltek',
       logo: '/i/sps/teltek.png',
-      link: 'https://teltek.es//?utm_origin=pulpocon&utm_medium=web'
+      link: 'https://teltek.es/?utm_origin=pulpocon&utm_medium=web'
     }
   ]
 }
